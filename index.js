f6ac7d353edfa006e0e71c9644723b74a81d1567
Caio Padovani <caiopadovaniamparo@gmail.com>
2023-09-30 12:01:15 -0300

    Minha segunda aula de codigo

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+// Use o body-parser para processar dados JSON nas solicitações POST e PUT
+app.use(bodyParser.json());
+
+app.get('/home', (req, res) => {
+    res.send('Selecione via caminho qual informacao. ');
+});
+
+app.get('/home/abner', (req, res) => {
+    res.send('Testa aquii e a tela de login com os dados da abner ');
+});
+
+app.get('/home/caio', (req, res) => {
+    res.send('Testa aquii e a tela de login com os dados da caio');
+});
+
+app.get('/home/bruna', (req, res) => {
+    res.send('Testa aquii e a tela de login com os dados da bruna ');
+});
+
+app.listen(port, () => {
+    console.log(`O servidor está rodando em http://localhost:${port}`);
+  });
